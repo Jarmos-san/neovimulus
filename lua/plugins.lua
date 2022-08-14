57576564155bd67662fbfa262b7fd699cee2c3c0
Somraj Saha <somraj.1994@gmail.com>
2022-08-14 17:52:30 +0530

    :sparkles: Move the plugins to the `lua/plugins` folder

---
+++ lua/plugins.lua
@@ -0,0 +1,27 @@
+local status, packer = pcall(require, "packer")
+
+if not status then
+	print("Packer not installed!")
+	return
+end
+
+vim.cmd([[ packadd packer.nvim ]])
+
+packer.startup({
+	function(use)
+		-- Allow "packer.nvim" to keep itself updated!
+		use("wbthomason/packer.nvim")
+
+		-- Install a good-looking colorscheme to make it easier to work with.
+		-- use("navarasu/onedark.nvim")
+	end,
+	config = {
+		display = {
+			open_fn = require("packer.util").float,
+		},
+		profile = {
+			enable = true,
+			threshold = 1,
+		},
+	},
+})
