754dfeaba381c423b68fc4165e4075db9655b3dd
Somraj Saha <somraj.1994@gmail.com>
2022-09-21 19:05:51 +0530

    :sparkles: Configure Neovim to install & setup the completions plugin properly

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -22,6 +22,53 @@ packer.startup({
 			end,
 		})
 
+        -- Install a bunch of plugins for better auto-completion & snippet support.
+        use({
+            "hrsh7th/nvim-cmp",
+            config = function()
+                local cmp = require("cmp")
+
+                cmp.setup({
+                    snippet = {
+                        expand = function(args)
+                            require("luasnip").lsp_expand(args.body)
+                        end,
+                    },
+                    window = {
+                        completion = cmp.config.window.bordered(),
+                        documentation = cmp.config.window.bordered(),
+                    },
+                    mapping = cmp.mapping.preset.insert({
+                        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                        ["<C-Space>"] = cmp.mapping.complete(),
+                        ["<C-e>"] = cmp.mapping.abort(),
+                        ["<CR>"] = cmp.mapping.confirm({ select = true}),
+                    }),
+                    sources = cmp.config.sources({
+                        { name = "nvim_lsp" },
+                        { name = "luasnip" },
+                        { name = "buffer" },
+                    }),
+                })
+
+            end,
+            requires = {
+                { "hrsh7th/cmp-nvim-lsp" },
+                { "hrsh7th/cmp-buffer" },
+                { "hrsh7th/cmp-path" },
+                { "hrsh7th/cmp-cmdline" },
+            },
+        })
+
+        -- Neccessary plugin for snippet support.
+        use({
+            "L3MON4D3/LuaSnip",
+            requires = {
+                { "saadparwaiz1/cmp_luasnip" },
+            },
+        })
+
 		-- Install plugin for configuring the LSP client.
 		use({
 			"neovim/nvim-lspconfig",
