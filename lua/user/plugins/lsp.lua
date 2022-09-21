54b03edac9bbd8de95cc3c27902bda42f3c42b29
Somraj Saha <somraj.1994@gmail.com>
2022-09-21 23:28:58 +0530

    :recycle: Add completion plugin's configurations to the `lsp` module

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -13,30 +13,58 @@ function M.setup_lsp()
 		vim.lsp.protocol.make_client_capabilities()
 	)
 
-    require("lspconfig").sumneko_lua.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        settings = {
-            Lua = {
-                runtime = {
-                    -- Set the correction version of the embedded Lua environment.
-                    version = "LuaJIT",
-                },
-                diagnostics = {
-                    -- configure the LSP server to understand the "vim" namespace.
-                    globals = { "vim" },
-                },
-                workspace = {
-                    -- configure the LSP server to understand where to look for Neovim runtime files.
-                    library = vim.api.nvim_get_runtime_file("", true)
-                },
-                telemetry = {
-                    -- Disable telemetry for privacy concerns.
-                    enable = false
-                },
-            },
-        },
-    })
+	require("lspconfig").sumneko_lua.setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+		settings = {
+			Lua = {
+				runtime = {
+					-- Set the correction version of the embedded Lua environment.
+					version = "LuaJIT",
+				},
+				diagnostics = {
+					-- configure the LSP server to understand the "vim" namespace.
+					globals = { "vim" },
+				},
+				workspace = {
+					-- configure the LSP server to understand where to look for Neovim runtime files.
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				telemetry = {
+					-- Disable telemetry for privacy concerns.
+					enable = false,
+				},
+			},
+		},
+	})
+end
+
+function M.setup_completions()
+	local cmp = require("cmp")
+
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				require("luasnip").lsp_expand(args.body)
+			end,
+		},
+		window = {
+			completion = cmp.config.window.bordered(),
+			documentation = cmp.config.window.bordered(),
+		},
+		mapping = cmp.mapping.preset.insert({
+			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-Space>"] = cmp.mapping.complete(),
+			["<C-e>"] = cmp.mapping.abort(),
+			["<CR>"] = cmp.mapping.confirm({ select = true }),
+		}),
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp" },
+			{ name = "luasnip" },
+			{ name = "buffer" },
+		}),
+	})
 end
 
 return M
