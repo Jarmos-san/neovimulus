ec04c06edad5e80caa3f77a130ee5e216a777d5f
Somraj Saha <somraj.1994@gmail.com>
2022-09-23 16:01:36 +0530

    :bulb: Add commented out section for the Treesitter plugin

--- lua/jarmos/plugins/init.lua
+++ lua/jarmos/plugins/init.lua
@@ -69,6 +69,33 @@ packer.startup({
 				require("jarmos.plugins.mason").install_servers()
 			end,
 		})
+
+		-- FIXME: Configure the Docker container with a C compiler for the plugin to work properly.
+		-- use({
+		--   "nvim-treesitter/nvim-treesitter",
+		--     run = function()
+		--       require("nvim-treesitter.install").update({
+		--         with_sync = true,
+		--       })
+		--     end,
+		--     config = function()
+		--       require("nvim-treesitter.configs").setup({
+		--         ensure_installed = { "lua" },
+		--         highlight = {
+		--           enable = true,
+		--         },
+		--         incremental_selection = {
+		--           enable = false,
+		--         },
+		--         indent = {
+		--           enable = false,
+		--         },
+		--         folding = {
+		--           enable = false,
+		--         },
+		--      })
+		--    end,
+		--  })
 	end,
 	config = {
 		display = {
