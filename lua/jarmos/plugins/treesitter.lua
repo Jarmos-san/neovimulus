c4e34df85140214185a38af28911db652130e4b3
Somraj Saha <somraj.1994@gmail.com>
2022-10-05 00:03:15 +0530

    :recycle: Configure Neovim to adjust with the newly added Treesitter plugins

--- lua/jarmos/plugins/treesitter.lua
+++ lua/jarmos/plugins/treesitter.lua
@@ -13,6 +13,18 @@ function M.setup()
 		incremental_selection = { enable = false },
 		indent = { enable = false },
 		folding = { enable = false },
+        -- INFO: Configurations for "bracket coloriser" plugin
+        -- https://github.com/p00f/nvim-ts-rainbow
+        rainbow = {
+            enable = true,
+            extended_mode = true,
+            max_file_lines = 1000,
+        },
+        -- INFO: Configurations for the autotag plugin
+        -- https://github.com/windwp/nvim-ts-autotag
+        autotag = {
+            enable = true,
+        },
 	})
 end
 
