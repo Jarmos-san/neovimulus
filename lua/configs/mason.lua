cab512644c0ef78d22e240be52ec1a708053fcd1
Somraj Saha <somraj.1994@gmail.com>
2023-02-11 22:56:44 +0530

    :sparkles: Add config module for the LSP installer plugin

---
+++ lua/configs/mason.lua
@@ -0,0 +1,4 @@
+-- FIXME: Check if the LSP servers installed using this plugin really works or not.
+require("mason").setup({
+    ui = { border = "rounded" }, -- Enable a nice-looking, rounded border for the UI.
+})
