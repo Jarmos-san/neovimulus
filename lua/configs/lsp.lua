0de1ddd4d2d56aa7b3f7255759a91d4c5faa6229
Somraj Saha <somraj.1994@gmail.com>
2023-02-11 22:56:12 +0530

    :sparkles: Add config module for the LSP plugin

---
+++ lua/configs/lsp.lua
@@ -0,0 +1,7 @@
+local lspconfig = require("lspconfig")
+
+vim.lsp.set_log_level("debug") -- This is necessary for the "nvim-lspconfig" plugin to report proper debug messages
+
+-- FIXME: Doesn't work for now.
+-- lspconfig["lua-language-server"].setup({})
+lspconfig["sumneko_lua"].setup({})
