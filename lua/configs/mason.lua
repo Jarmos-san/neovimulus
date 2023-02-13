569cf98360c6dc992675d7213e737907e66d056f
Somraj Saha <somraj.1994@gmail.com>
2023-02-14 00:04:09 +0530

    :recycle: Reconfigure the `mason` plugin a bit

--- lua/configs/mason.lua
+++ lua/configs/mason.lua
@@ -1,4 +1,5 @@
 -- FIXME: Check if the LSP servers installed using this plugin really works or not.
 require("mason").setup({
     ui = { border = "rounded" }, -- Enable a nice-looking, rounded border for the UI.
+    log_level = vim.log.levels.DEBUG -- Enable better debugging info for the LSP installer.
 })
