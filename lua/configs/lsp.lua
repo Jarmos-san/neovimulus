b68798ce3bc95645fb0c3cca62aeb3c2bfdcced3
Somraj Saha <somraj.1994@gmail.com>
2023-02-22 23:32:28 +0530

    :bug: Fix some keybinding issues with the LSPs

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -16,7 +16,20 @@ local on_attach = function()
     local opts = { noremap = true, silent = true, bufnr = bufnr }
 
     map("n", "gD", vim.lsp.buf.declaration, opts)
+    map("n", "gd", vim.lsp.buf.definition, opts)
+    map("n", "K", vim.lsp.buf.hover, opts)
+    map("n", "gi", vim.lsp.buf.implementation, opts)
+    map("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+    map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    map("n", "<Leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folder)) end, opts)
+    map("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+    map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    map("n", "gr", vim.lsp.buf.references, opts)
     
+    -- Configurations for showing diagnostics in a hover window instead. See the documentations at:
+    -- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#show-line-diagnostics-automatically-in-hover-window
     vim.api.nvim_create_autocmd("CursorHold", {
         buffer = bufnr,
         callback = function()
