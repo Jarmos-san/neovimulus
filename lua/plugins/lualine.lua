cdd0b32c836b57e2cf0b7331cfc1a7ac9260d74f
Somraj Saha <somraj.1994@gmail.com>
2023-02-23 14:56:51 +0530

    :sparkles: Add a configuration for the statusline plugin

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "auto",
+        globalstatus = true,
+        disabled_filetypes = {
+          statusline = { "dashboard", "lazy", "alpha" },
+        },
+      },
+    },
+  },
+}
