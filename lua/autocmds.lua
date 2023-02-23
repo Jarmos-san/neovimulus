c1751ad22b04ff93cab8e9ae527ac7fcd1b12393
Somraj Saha <somraj.1994@gmail.com>
2023-02-23 14:14:16 +0530

    :poop: Fix some formatting issues & add some more autocommands

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,78 +1,98 @@
 local autocmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
+local augroup = function(name)
+    return vim.api.nvim_create_augroup("augroup" .. name, { clear = true })
+end
 
 autocmd("TextYankPost", {
-	desc = "Highlight the yanked text for a specified time.",
-	group = augroup("yank_highlight", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank({ timeout = 250 }) -- Keep the highlight for 250ms after yanking.
-	end,
+  desc = "Highlight the yanked text for a specified time.",
+  group = augroup("yank_highlight"),
+  callback = function()
+    vim.highlight.on_yank({ timeout = 250 }) -- Keep the highlight for 250ms after yanking.
+  end,
 })
 
 autocmd("FileType", {
-    desc = "When opening Git commit messages, start the buffer in Insert mode",
-    group = augroup("git_insert", { clear = true }),
-    pattern = { "gitcommit", "gitrebase" },
-    command = "startinsert | 1"
+  desc = "When opening Git commit messages, start the buffer in Insert mode",
+  group = augroup("git_insert"),
+  pattern = { "gitcommit", "gitrebase" },
+  command = "startinsert | 1",
 })
 
 autocmd("FocusLost", {
-    desc = "Save/write all unsaved buffers when focus is lost",
-    group = augroup("save_buffers", { clear = true }),
-    pattern = "*",
-    command = "silent! wall",
+  desc = "Save/write all unsaved buffers when focus is lost",
+  group = augroup("save_buffers"),
+  pattern = "*",
+  command = "silent! wall",
 })
 
 autocmd("BufReadPost", {
-    desc = "Jump to the last known position of a file before closing it",
-    group = augroup("buffer_checkpoint", { clear = true }),
-    callback = function()
-        local mark = vim.api.nvim_buf_get_mark(0, '"')
-        local line_count = vim.api.nvim_buf_line_count(0)
+  desc = "Jump to the last known position of a file before closing it",
+  group = augroup("buffer_checkpoint"),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local line_count = vim.api.nvim_buf_line_count(0)
 
-        if mark[1] > 0 and mark[1] <= line_count then
-            pcall(vim.api.nvim_win_set_cursor, 0, mark)
-        end
+    if mark[1] > 0 and mark[1] <= line_count then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
     end
+  end,
 })
 
 -- INFO: See the documentations below for an explanation on it works:
 -- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim#events
 autocmd("User", {
-    desc = "Print a helpful message when Mason is about to install the LSP servers",
-    group = augroup("mason", { clear = true }),
-    pattern = "MasonToolsStartingInstall",
-    callback = function()
-        vim.schedule(function()
-            print("Mason is starting...")
-        end)
-    end,
+  desc = "Print a helpful message when Mason is about to install the LSP servers",
+  group = augroup("mason"),
+  pattern = "MasonToolsStartingInstall",
+  callback = function()
+    vim.schedule(function()
+      print("Mason is starting...")
+    end)
+  end,
 })
 
 autocmd("User", {
-    desc = "Print a helpful message when Mason is done updating the LSP servers",
-    group = augroup("mason", { clear = true }),
-    pattern = "MasonToolsUpdateCompleted",
-    callback = function()
-        vim.schedule(function()
-            print("Mason completed updating LSP servers...")
-        end)
-    end,
+  desc = "Print a helpful message when Mason is done updating the LSP servers",
+  group = augroup("mason"),
+  pattern = "MasonToolsUpdateCompleted",
+  callback = function()
+    vim.schedule(function()
+      print("Mason completed updating LSP servers...")
+    end)
+  end,
 })
 
 autocmd("User", {
-    desc = "Open the Alpha dashboard when the last buffer is closed instead of an empty buffer",
-    group = augroup("alpha_on_empty", { clear = true }),
-    pattern = "BDeletePre*",
+  desc = "Open the Alpha dashboard when the last buffer is closed instead of an empty buffer",
+  group = augroup("alpha_on_empty"),
+  pattern = "BDeletePre*",
+  callback = function(event)
+    local fallback_name = vim.api.nvim_buf_get_name(event.buf)
+    local fallback_filetype = vim.api.nvim_buf_get_option(event.buf, "filetype")
+    local fallback_on_empty = fallback_name == "" and fallback_filetype == ""
+
+    if fallback_on_empty then
+      vim.cmd("Neotree close")
+      vim.cmd("Alpha")
+      vim.cmd(event.buf .. "bwipeout")
+    end
+  end,
+})
+
+autocmd("VimResized", {
+    desc = "Resize the splits if the window is resized",
+    group = augroup("resize_splits"),
+    callback= function()
+        vim.cmd("tabdo wincmd =")
+    end
+})
+
+autocmd("FileType", {
+    desc = "Close some filtypes simply by pressing 'q'",
+    group = augroup("close_with_q"),
+    pattern = { "qf", "help", "man", "notify", "lspinfo", "tsplayground" },
     callback = function(event)
-        local fallabck_name = vim.api.nvim_buf_get_name(event.buf)
-        local fallback_filetype = vim.api.nvim_buf_get_option(event.buf, "filetype")
-        local fallback_on_empty = fallback_name == "" and fallback_filetype == ""
-
-        if fallback_on_empty then
-            vim.cmd("Neotree close")
-            vim.cmd("Alpha")
-            vim.cmd(event.buf .. "bwipeout")
-        end
-    end,
+        vim.bo[event.buf].buflisted = false
+        vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+    end
 })
