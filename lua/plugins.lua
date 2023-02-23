8ef0ad15dd8cbc3a9de23c6cdd8fd98099e82028
Somraj Saha <somraj.1994@gmail.com>
2023-02-23 21:20:27 +0530

    :recycle: Move some of the plugins to separate modules

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -97,97 +97,8 @@ return {
   },
 
   {
-    -- Plugin for more easily commenting out blocks of code.
-    "numToStr/Comment.nvim",
-    event = "BufReadPost", -- Load the plugin only after the buffer is read.
-    config = true, -- Setup the plugin for it to be initialised & work.
-  },
-
-  {
-    -- Plugin for automatically inserting bracket pairs & much more.
-    "windwp/nvim-autopairs",
-    event = "InsertEnter", -- Load the plugin only when entering Insert mode.
-    config = true, -- Setup the plugin to be initialised & work as expected.
-  },
-
-  {
-    -- Plugin for properly visualising the indent lines & much more.
-    "lukas-reineke/indent-blankline.nvim",
-    event = "BufReadPost", -- Load the plugin only the buffer is read.
-    config = function()
-      require("configs.indent-blankline") -- Configuration module for the plugin.
-    end,
-  },
-
-  -- {
-  --     "famiu/bufdelete.nvim",
-  --     disable = true, -- FIXME: Doesn't work for now.
-  --     cmd = "BDelete",
-  --     config = function()
-  --         require("bufdelete").bufdelete(0, true)
-  --     end,
-  -- },
-
-  -- {
-  --     "folke/which-key.nvim", -- Plugin for easier maintainance of keybindings.
-  --     event = "BufReadPost", -- Load the plugin only after the contents of the buffer is read.
-  --     config = function()
-  --         require("configs.which-key") -- Configuraiont module for the plugin.
-  --     end,
-  -- },
-
-  {
-    "norcalli/nvim-colorizer.lua", -- Plugin to display the colours of their respective hex codes.
-    ft = { "scss", "css", "javascript", "typescriptreact", "html" }, -- Specific filetypes to load this plugin for.
-    config = true, -- Initializes the plugin for usage within Neovim.
-  },
-
-  {
-    "b0o/schemastore.nvim", -- Plugin to load JSON schemas.
-    ft = "json", -- Load the plugin only for JSON files.
-  },
-
-  {
-    "karb94/neoscroll.nvim", -- Plugin for smooth scrolling across a buffer.
-    event = "BufReadPost", -- Load plugin only after the contents of the buffer are read
-    config = true, -- Initialise the plugin for usage within Neovim.
-  },
-
-  {
-    "roobert/search-replace.nvim", -- Plugin for search & replace capabilities.
-    event = "BufReadPost", -- Load the plugin only after the contents of the buffer are read.
-    config = function()
-      require("configs.search-replace") -- Configuration module for the plugin.
-    end,
-  },
-
-  {
-    -- Plugin for a better colorcolumn, this replaces the need for using filetype-based plugin config.
-    "m4xshen/smartcolumn.nvim",
-    event = "FileType", -- Load the plugin only when the filetype of the buffer is recognised.
-    config = function()
-      require("smartcolumn").setup({
-        disabled_filetypes = { "help", "text", "markdown", "alpha" },
-        custom_colorcolumn = { lua = 120 }, -- Configure the character length at which to show the colorcolumn.
-      })
-    end,
-  },
-
-  {
-    "max397574/better-escape.nvim", -- Plugin for a better & quicker "Escape" mechanism.
-    event = "BufReadPost", -- TODO: Figure out an event to load the plugin even later.
-    config = true,
-  },
-
-  {
     "rcarriga/nvim-notify", -- A plugin which makes the Neovim UI prettier & more modern.
     config = true,
-  },
-
-  {
-    "akinsho/toggleterm.nvim",
-    config = true,
-    cmd = { "ToggleTerm" },
   },
 
   {
@@ -201,12 +112,5 @@ return {
     "folke/noice.nvim",
     config = true,
     dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
-  },
-
-  {
-    "chrisgrieser/nvim-various-textobjs",
-    config = function()
-      require("various-textobjs").setup({ useDefaultKeymaps = true })
-    end,
   },
 }
