a7c83c4d6d0e4a62b48203c7429cabd208e6fc40
Somraj Saha <somraj.1994@gmail.com>
2023-02-24 23:54:56 +0530

    :fire: Remove some redundant files

--- lua/configs/indent-blankline.lua
+++
@@ -1,7 +0,0 @@
-require("indent_blankline").setup({
-    char = "┊",
-    space_char_blankline = " ",
-    show_end_of_line = true,
-    show_trailing_blankline_indent = true,
-    show_current_context = true,
-})
