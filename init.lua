0a6e8f3b30db98f750c4db6d7fad4565cac79b75
Somraj Saha <somraj.1994@gmail.com>
2023-02-25 19:48:25 +0530

    :bug: Fix some module import issues

--- init.lua
+++ init.lua
@@ -51,8 +51,8 @@ require("lazy").setup("plugins", {
 })
 
 -- Load the necessary user-defined Lua modules here for customising Neovim.
-require("options")
-require("autocmds")
-require("keymaps")
+require("configs.options")
+require("configs.autocmds")
+require("configs.keymaps")
 
 vim.cmd([[ colorscheme onedark ]])
