685a3c368f337dcd4754c5cd296920f3ae42db09
Somraj Saha <somraj.1994@gmail.com>
2023-02-26 23:40:07 +0530

    :recycle: Refactor the lualine plugin configurations for a better look

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,16 +2,27 @@
 
 return {
   {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = {
-      options = {
-        theme = "auto",
-        globalstatus = true,
-        disabled_filetypes = {
-          statusline = { "filesytem", "neo-tree", "dashboard", "lazy", "alpha" },
+    "nvim-lualine/lualine.nvim", -- Plugin for configuring a nice looking statusline
+    event = "VeryLazy", -- Load the plugin when Lazy wants it to
+    config = function()
+      require("lualine").setup({
+        options = {
+          -- Leaving an empty table renders the square-edged components, else the default angled ones are loaded
+          section_separators = {},
+          component_separator = "|",
+          theme = "codedark", -- Set the theme
+          globalstatus = true,
+          disabled_filetypes = { -- Disable the statusline for certain filetypes mentioned below
+            statusline = { "filesytem", "neo-tree", "dashboard", "lazy", "alpha" },
+          },
         },
-      },
-    },
+        sections = {
+          -- Statusline components to showcase on the right-most end
+          lualine_x = { "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+      })
+    end,
   },
 }
