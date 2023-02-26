5a1b672173d576ed400a201e5d610556180fc95a
Somraj Saha <somraj.1994@gmail.com>
2023-02-26 23:38:40 +0530

    :recycle: Leave comments & refactor a bit

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -2,11 +2,13 @@
 
 return {
   {
-    "goolord/alpha-nvim",
-    event = "VimEnter",
+    "goolord/alpha-nvim", -- Plugin to load a nice dashboard with utilities in the startup screen
+    event = "VimEnter", -- Load the plugin right after entering the UI
     opts = function()
+      -- Load the configurations for the Alpha dashboard
       local dashboard = require("alpha.themes.dashboard")
 
+      -- Configure some utilitarian buttons to show on the dashboard
       dashboard.section.buttons.val = {
         dashboard.button("n", " " .. " Create & Open a New File", ":ene <BAR> startinsert <CR>"),
         dashboard.button("n", " " .. " Open the File Explorer", "<CMD>Neotree toggle<CR>"),
@@ -15,6 +17,7 @@ return {
         dashboard.button("q", " " .. " Quit Out of Neovim", ":qa<CR>"),
       }
 
+      -- Configure some highlight groups for the utilitarian buttons mentioned above
       for _, button in ipairs(dashboard.section.buttons.val) do
         button.opts.hl = "AlphaButtons"
         button.opts.hl_shortcut = "AlphaShortcut"
@@ -28,9 +31,11 @@ return {
       return dashboard
     end,
     config = function(_, dashboard)
+      -- Logic to show Lazy stats on the Alpha dashboard properly
       if vim.o.filetype == "lazy" then
         vim.cmd.close()
         vim.api.nvim_create_autocmd("User", {
+          group = vim.api.nvim_create_augroup("show_lazy_on_alpha_ready", { clear = true }),
           pattern = "AlphaReady",
           callback = function()
             require("lazy").show()
@@ -38,9 +43,11 @@ return {
         })
       end
 
-      require("alpha").setup(dashboard.opts)
+      require("alpha").setup(dashboard.opts) -- Initiliase the Alpha dashboard with the configurations above
 
+      -- Autocommand to show load times of all the installed plugins right within the Alpha dashboard
       vim.api.nvim_create_autocmd("User", {
+        group = vim.api.nvim_create_augroup("show_lazy_stats_on_alpha", { clear = true }),
         pattern = "LazyVimStarted",
         callback = function()
           local stats = require("lazy").stats()
