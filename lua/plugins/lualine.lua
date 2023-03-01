abb602e9a5c145faacb31f0c4ff8738086ffe855
Somraj Saha <somraj.1994@gmail.com>
2023-03-01 10:26:29 +0530

    :recycle: Reconfigure the statusline to show LSP progress

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,7 +8,7 @@ return {
       require("lualine").setup({
         options = {
           -- Leaving an empty table renders the square-edged components, else the default angled ones are loaded
-          section_separators = {},
+          section_separators = "",
           component_separator = "|",
           theme = "codedark", -- Set the theme
           globalstatus = true,
@@ -17,12 +17,14 @@ return {
           },
         },
         sections = {
+          lualine_c = "lsp_progress",
           -- Statusline components to showcase on the right-most end
-          lualine_x = { "filetype" },
-          lualine_y = { "progress" },
-          lualine_z = { "location" },
+          lualine_x = "filetype",
+          lualine_y = "progress",
+          lualine_z = "location",
         },
       })
     end,
   },
+  dependencies = { "WhoIsSethDaniel/lualine-lsp-progress.nvim" }, -- Plugin to show the LSP loading status/progress
 }
