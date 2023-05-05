a28660612718cad27453a28fe32a32813c7c7abe
Somraj Saha <somraj.1994@gmail.com>
2023-05-05 11:02:16 +0530

    :sparkles: Add some bare minimum autocommands

--- neovimulus/lua/autocmds.lua
+++ neovimulus/lua/autocmds.lua
@@ -1,3 +1,5 @@
+-- Module for configuring various global autocommands for Neovim
+--
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = function(name)
   return vim.api.nvim_create_augroup("augroup" .. name, { clear = true })
@@ -38,47 +40,6 @@ autocmd("BufReadPost", {
   end,
 })
 
--- INFO: See the documentations below for an explanation on it works:
--- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim#events
-autocmd("User", {
-  desc = "Print a helpful message when Mason is about to install the LSP servers",
-  group = augroup("mason"),
-  pattern = "MasonToolsStartingInstall",
-  callback = function()
-    vim.schedule(function()
-      print("Mason is starting...")
-    end)
-  end,
-})
-
-autocmd("User", {
-  desc = "Print a helpful message when Mason is done updating the LSP servers",
-  group = augroup("mason"),
-  pattern = "MasonToolsUpdateCompleted",
-  callback = function()
-    vim.schedule(function()
-      print("Mason completed updating LSP servers...")
-    end)
-  end,
-})
-
-autocmd("User", {
-  desc = "Open Alpha dashboard when all buffers are removed",
-  group = augroup("open_alpha_on_buffer_removal"),
-  pattern = "BDeletePost*",
-  callback = function(event)
-    local fallback_name = vim.api.nvim_buf_get_name(event.buf)
-    local fallback_filetype = vim.api.nvim_buf_get_option(event.buf, "filetype")
-    local fallback_on_empty = fallback_name == "" and fallback_filetype == ""
-
-    if fallback_on_empty then
-      vim.cmd("Neotree close")
-      vim.cmd("Alpha")
-      vim.cmd(event.buf .. "bwipeout")
-    end
-  end,
-})
-
 autocmd("VimResized", {
   desc = "Resize the splits if the window is resized",
   group = augroup("resize_splits"),
@@ -94,5 +55,15 @@ autocmd("FileType", {
   callback = function(event)
     vim.bo[event.buf].buflisted = false
     vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+autocmd({ "CursorMoved", "CursorMovedI" }, {
+  desc = "Redraw the cursorline when navigating around the buffer",
+  group = augroup("cursorline_number"),
+  callback = function()
+    if vim.wo.cursorline then
+      vim.cmd("redraw")
+    end
   end,
 })
